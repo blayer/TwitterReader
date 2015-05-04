4b4babecbd51fa3b19c14728ccea106e00066548
Changkun Zhao <changkunzhao@Changkuns-Air.home>
2015-05-04 18:57:12 -0400

    Initial Commit

---
+++ TwitterReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwitterReader
+//
+//  Created by Changkun Zhao on 5/4/15.
+//  Copyright (c) 2015 Changkun Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TwitterReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TwitterReader
+//
+//  Created by Changkun Zhao on 5/4/15.
+//  Copyright (c) 2015 Changkun Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
